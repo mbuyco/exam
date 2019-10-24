3c08d642eb29742c103fa481938c87617706adc4
mbuyco <buyco.michael@gmail.com>
2019-10-24 16:46:25 +0800

    added exam seeder

--- database/seeds/ExamsTableSeeder.php
+++ database/seeds/ExamsTableSeeder.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class ExamsTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-      $this->call([ExamsTableSeeder::class]);
+      factory(App\Exam::class, 10)->create();
     }
 }
